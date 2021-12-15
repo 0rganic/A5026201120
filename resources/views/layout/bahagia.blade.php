34b3851aa6b3641ab1de2d60eae6d08375cc8e05
Jeremy Diaz Prasetyo <jeremydiazp@gmail.com>
2021-12-15 13:34:31 +0700

    script pemisah ribuan

--- resources/views/layout/bahagia.blade.php
+++ resources/views/layout/bahagia.blade.php
@@ -54,6 +54,14 @@
         });
     </script>
 
+    <script>
+        function rupiah($angka){
+
+        $hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+        return $hasil_rupiah;
+
+        }
+    </script>
 
     <style>
         /* footer */
